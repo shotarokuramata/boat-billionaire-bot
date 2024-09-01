7ff11851689d987787412887912a77bdd90b5a52
shotarokuramata <shokuramata@gmail.com>
2024-09-01 14:05:44 +0900

    me-ru

---
+++ mail.py
@@ -0,0 +1,21 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.utils import formatdate
+import json
+
+def send_mail(subject, body):
+    # JSONファイルを読み込む
+    with open('config.json', 'r') as f:
+        config = json.load(f)
+
+    from_address = config['from_address']
+    to_address = config['to_address']
+    password = config['password']
+
+    # SMTPサーバに接続
+    smtpobj = smtplib.SMTP('smtp.gmail.com', 587)
+    smtpobj.starttls()
+    smtpobj.login(from_address, password)
+    # 作成したメールを送信
+    smtpobj.sendmail(from_address, to_address, 'Subject: ' + subject + '\n' + body)
+    smtpobj.close()
