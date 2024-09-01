98a45744bf838e75a808f748407f13cbac7668c2
shotarokuramata <shokuramata@gmail.com>
2024-09-01 14:14:09 +0900

    メール修正

--- mail.py
+++ mail.py
@@ -12,10 +12,18 @@ def send_mail(subject, body):
     to_address = config['to_address']
     password = config['password']
 
+    msg = MIMEText(body, 'plain', 'utf-8')
+    msg['Subject'] = subject
+    msg['From'] = from_address
+    msg['To'] = to_address
+    msg['Date'] = formatdate(localtime=True)
+
+
     # SMTPサーバに接続
     smtpobj = smtplib.SMTP('smtp.gmail.com', 587)
     smtpobj.starttls()
     smtpobj.login(from_address, password)
     # 作成したメールを送信
-    smtpobj.sendmail(from_address, to_address, 'Subject: ' + subject + '\n' + body)
+    smtpobj.sendmail(from_address, to_address, msg.as_string())
     smtpobj.close()
+
